12fb4cb5ee475cac143eec21aa19ddd19388b8d9
Seheon Yu <developer@seheon.kr>
2023-06-04 20:10:03 +0900

    Update routers and links
    + routers.tsx
    * link.ts (`a` tag to react-router-dom Link)

--- src/components/atoms/link.ts
+++ src/components/atoms/link.ts
@@ -1,6 +1,8 @@
+import { Link as RouterLink } from 'react-router-dom';
+
 import styled from 'styled-components';
 
-export const Link = styled.a`
+export const Link = styled(RouterLink)`
   font-size: 1rem;
   text-decoration: none;
   color: ${({ theme }) => theme.textPrimary};
